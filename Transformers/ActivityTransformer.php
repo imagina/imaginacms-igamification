bcf905418eeae33b09187bf31b6891646791e670
William Verde <williamverde10@gmail.com>
2022-03-03 10:51:17 -0400

    Update: activity transformer

--- Transformers/ActivityTransformer.php
+++ Transformers/ActivityTransformer.php
@@ -7,14 +7,29 @@ use Modules\Core\Icrud\Transformers\CrudResource;
 class ActivityTransformer extends CrudResource
 {
   /**
-  * Method to merge values with response
-  *
-  * @return array
-  */
+   * Method to merge values with response
+   *
+   * @return array
+   */
   public function modelAttributes($request)
   {
-    return [
+    //Get request Filters
+    $filter = json_decode($request->filter);
+
+    //Instance data
+    $data = [
       'statusName' => $this->statusName,
     ];
+
+    if (isset($filter->validateComplete)) {
+      $userId = \Auth::user()->id;
+      $user = $this->users->where('id', $userId)->first();
+
+      $data['userId'] = $userId;
+      $data['userCompleted'] = $user ? true : false;
+    }
+
+    //Response
+    return $data;
   }
 }
