a11e90b85c55011fb024daf593233d59f771f026
msolano <msolano@imaginacolombia.com>
2022-03-02 19:32:33 -0500

    +Add: cms translations

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'sidebar' => [
+    'adminGroup' => 'Gamification',
+    'adminActivities' => "Activities",
+    'adminCategories' => 'Categories'
+  ],
+  'newActivity' => 'New Activity',
+  'updateActivity' => 'Update Activity',
+  'newCategory' => 'New Category',
+  'updateCategory' => 'Update Category',
+];
---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'sidebar' => [
+    'adminGroup' => 'Gamificación',
+    'adminActivities' => "Actividades",
+    'adminCategories' => 'Categorias'
+  ],
+  'newActivity' => 'Nueva Actividad',
+  'updateActivity' => 'Actualizar Actividad',
+  'newCategory' => 'Nueva Categoría',
+  'updateCategory' => 'Actualizar Categoría',
+];
