df19b7a29aba179912ab03d727871924a6381547
imagina <msolano2505@gmail.com>
2023-01-17 18:01:54 -0500

    +Add: Settings for HelpCenter

--- Config/settings-fields.php
+++ Config/settings-fields.php
@@ -1,5 +1,56 @@
 <?php
 
 return [
-
+  //Help Center
+  'hcStatus' => [
+    "onlySuperAdmin" => true,
+    'name' => 'igamification::hcStatus',
+    'value' => "1",
+    'type' => 'select',
+    'groupName' => 'helpCenter',
+    'groupTitle' => 'igamification::common.settingGroups.helpCenter',
+    'columns' => 'col-12 col-md-6',
+    'props' => [
+      'label' => 'igamification::common.settings.helpCenter.status',
+      'options' => [
+        ['label' => 'isite::common.yes', 'value' => '1'],
+        ['label' => 'isite::common.no', 'value' => '0'],
+      ]
+    ],
+  ],
+  'hcInternalCategory' => [
+    "onlySuperAdmin" => true,
+    'name' => 'igamification::hcInternalCategory',
+    'value' => null,
+    'type' => 'select',
+    'groupName' => 'helpCenter',
+    'groupTitle' => 'igamification::common.settingGroups.helpCenter',
+    'columns' => 'col-12 col-md-6',
+    'help' => [
+      "description" => 'igamification::common.settings.helpCenter.internalCategoryDescription'
+    ],
+    'props' => [
+      'label' => 'igamification::common.settings.helpCenter.internalCategory',
+      'clearable' => true
+    ],
+    'loadOptions' => [
+      'apiRoute' => 'apiRoutes.qgamification.categories'
+    ]
+  ],
+  'hcCentralizedSource' => [
+    "onlySuperAdmin" => true,
+    'name' => 'igamification::hcCentralizedSource',
+    'value' => "https://www.imaginacolombia.com",
+    'type' => 'input',
+    'groupName' => 'helpCenter',
+    'groupTitle' => 'igamification::common.settingGroups.helpCenter',
+    'columns' => 'col-12',
+    'required' => true,
+    'help' => [
+      "description" => 'igamification::common.settings.helpCenter.centralizedSourceDescription'
+    ],
+    'props' => [
+      'label' => 'igamification::common.settings.helpCenter.centralizedSource',
+    ]
+  ],
 ];
