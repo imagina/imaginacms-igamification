97fbb01728d7b909bfe10ce852cb1ed83ed2bb5c
imagina <msolano2505@gmail.com>
2023-01-25 16:34:01 -0500

    +Update: activity transformer to return more form data

--- Transformers/ActivityTransformer.php
+++ Transformers/ActivityTransformer.php
@@ -18,27 +18,36 @@ class ActivityTransformer extends CrudResource
     //Get request Filters
     $filter = json_decode($request->filter);
 
-    // esta sección de código se agregó porque el formeable empezó a dar problemas cuando se implementó el tenant
-    // la idea es que los formularios pertenezcan a un tenant también pero en el caso del formulario que pertenece al
-    // registro que es un formulario central no llega con el trait porque el trait utiliza una relación morphMany que
-    // ejecuta el scope del Tenant y no hay forma de evitarlo, por eso se realizó esta adaptación: se busca el id del
-    // formulario asociado en la tabla formeable y luego se busca el objeto completo pero a través del repositorio
-    $formRepository = app("Modules\Iforms\Repositories\FormRepository");
-    $formeable = \DB::table("iforms__formeable")
-      ->where("formeable_type", "Modules\\Igamification\\Entities\\Activity")
-      ->where("formeable_id", $this->id)
-      ->first();
-
-    $form = isset($formeable->form_id) ? $formRepository->getItem($formeable->form_id) : null;
-
     //Instance data
     $data = [
       'statusName' => $this->statusName,
       'type' => $this->type ?? Type::DEFAULT_TYPE,
       'typeName' => $this->typeName,
-      'form' => isset($form->id) ? new FormTransformer($form) : null,
-      'formId' => $form->id ?? null,
+      'form' => null,
+      'formId' => null
     ];
+
+    // esta sección de código se agregó porque el formeable empezó a dar problemas cuando se implementó el tenant
+    // la idea es que los formularios pertenezcan a un tenant también pero en el caso del formulario que pertenece al
+    // registro que es un formulario central no llega con el trait porque el trait utiliza una relación morphMany que
+    // ejecuta el scope del Tenant y no hay forma de evitarlo, por eso se realizó esta adaptación: se busca el id del
+    // formulario asociado en la tabla formeable y luego se busca el objeto completo pero a través del repositorio
+    if ($this->type == Type::INTERNAL_FORM) {
+      $formRepository = app("Modules\Iforms\Entities\Form");
+      $form = $formRepository->where('id', function ($subQ) {
+        $subQ->from('iforms__formeable')
+          ->select(['form_id'])
+          ->where("formeable_type", "Modules\\Igamification\\Entities\\Activity")
+          ->where("formeable_id", $this->id)
+          ->first();
+      })->with('translations', 'fields')->first();
+
+      //Instance data
+      $data = [
+        'form' => isset($form->id) ? new FormTransformer($form) : null,
+        'formId' => $form->id ?? null,
+      ];
+    }
 
     if (isset($filter->validateComplete)) {
       $user = \Auth::user();
