21e5439188ded1d7b7cfee0f4cb7dbec445ad0b5
imagina <msolano2505@gmail.com>
2023-03-31 09:51:09 -0500

    +Update: Run seeds as jobs

--- Database/Seeders/IgamificationDatabaseSeeder.php
+++ Database/Seeders/IgamificationDatabaseSeeder.php
@@ -5,18 +5,20 @@ namespace Modules\Igamification\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use Modules\Isite\Jobs\ProcessSeeds;
+
 class IgamificationDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(IgamificationModuleTableSeeder::class);
-        $this->call(IgamificationSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Igamification\Database\Seeders",
+      "seeds" => ["IgamificationModuleTableSeeder", "IgamificationSeeder"]
+    ]);
+  }
 }
