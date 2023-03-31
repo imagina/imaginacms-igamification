721498519498fe2c2245ac3100bb5280a6e04ad4
imagina <msolano2505@gmail.com>
2023-03-31 15:18:16 -0500

    +Update: Migrations to fix the migrate-refresh

--- Database/Migrations/2022_02_28_103106491753_create_igamification_categories_table.php
+++ Database/Migrations/2022_02_28_103106491753_create_igamification_categories_table.php
@@ -5,34 +5,35 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateIgamificationCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('igamification__categories', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            // Your fields...
-            $table->string('system_name')->unique()->nullable();
-            $table->integer('parent_id')->default(0)->nullable();
-            $table->text('options')->nullable();
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('igamification__categories', function (Blueprint $table) {
+      $table->engine = 'InnoDB';
+      $table->increments('id');
+      // Your fields...
+      $table->string('system_name')->unique()->nullable();
+      $table->integer('parent_id')->default(0)->nullable();
+      $table->text('options')->nullable();
 
-            // Audit fields
-            $table->timestamps();
-            $table->auditStamps();
-        });
-    }
+      // Audit fields
+      $table->timestamps();
+      $table->auditStamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('igamification__categories');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('igamification__category_user');
+    Schema::dropIfExists('igamification__categories');
+  }
 }
